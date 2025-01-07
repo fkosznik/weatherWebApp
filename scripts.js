33dc2410558fdd185a18cc259026745f0852b1ee
filipkosznik <filip.kosznik@gmail.com>
2025-01-07 18:16:04 +0100

    o

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
-const input = document.getElementById("input");
-const jsonDescription = document.getElementById("description");
-const jsonTemp = document.getElementById("temp");
-const jsonIcon = document.getElementById("icon");
-const wrong = document.getElementById("error");
-const jsonMin = document.getElementById("min");
-const jsonMax = document.getElementById("max");
-const jsonHumiduty = document.getElementById("humidity");
-const jsonCloudy = document.getElementById("cloudy");
-const jsonWindy = document.getElementById("windy");
-const inputLocation = document.getElementById("location");
+const input = document.getElementById('input');
+const jsonDescription = document.getElementById('description');
+const jsonTemp = document.getElementById('temp');
+const jsonIcon = document.getElementById('icon');
+const wrong = document.getElementById('error');
+const jsonMin = document.getElementById('min');
+const jsonMax = document.getElementById('max');
+const jsonHumiduty = document.getElementById('humidity');
+const jsonCloudy = document.getElementById('cloudy');
+const jsonWindy = document.getElementById('windy');
+const inputLocation = document.getElementById('location');
 
-
-//get json from api and dsplay results
 async function getData(place) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&include=current&key=FQP2G8FQV52GSV7WP8S8EECL9&contentType=json`;
   try {
@@ -24,32 +22,31 @@ async function getData(place) {
 
     const weatherData = new WeatherData(json);
     const today = weatherData.days[1];
-    
 
     jsonDescription.textContent = today.description;
-     jsonMax.textContent = today.tempmax + "°C";
-     jsonMin.textContent = today.tempmin+ "°C";
-     jsonHumiduty.textContent = today.humidity+"%";
-    jsonCloudy.textContent = today.cloudcover+ "%";
-     jsonWindy.textContent = today.windspeed+"km/h";
-    jsonTemp.textContent = today.temp + "°C";
+    jsonMax.textContent = today.tempmax + '°C';
+    jsonMin.textContent = today.tempmin + '°C';
+    jsonHumiduty.textContent = today.humidity + '%';
+    jsonCloudy.textContent = today.cloudcover + '%';
+    jsonWindy.textContent = today.windspeed + 'km/h';
+    jsonTemp.textContent = today.temp + '°C';
     jsonIcon.src = `images/${today.icon}.png`;
-    
-    inputLocation.textContent=weatherData.resolvedAddress
+
+    inputLocation.textContent = weatherData.resolvedAddress;
     const weatherMain = today.icon;
-    
+
     changeBackgroundImg(weatherMain);
   } catch (error) {
-    if(error.message=="Response status: 400"){
-    window.alert("Can't find this location, try different one.");
-    console.error(error.message);
+    if (error.message == 'Response status: 400') {
+      window.alert("Can't find this location, try different one.");
+      console.error(error.message);
+    } else {
+      console.error(error.message);
     }
-    else{console.error(error.message);}
   }
 }
 function call() {
   getData(input.value);
-  
 }
 
 class WeatherData {
@@ -106,102 +103,91 @@ class Day {
   }
 }
 
-
-
-
-
 //change bg depending on weather
 
 function changeBackgroundImg(weatherMain) {
   switch (weatherMain) {
-
-    case "partly-cloudy-day":
+    case 'partly-cloudy-day':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/partly-cloudy-day.jpg")`
       );
       break;
-    case "clear-day":
+    case 'clear-day':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/clear-day.jpg")`
       );
       break;
-    case "clear-night":
+    case 'clear-night':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/clear-night.jpg")`
       );
       break;
-  
-    case "cloudy":
+
+    case 'cloudy':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/cloudy.jpg")`
       );
       break;
-    case "wind":
+    case 'wind':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/wind.jpg")`
       );
       break;
-    case "partly-cloudy-night":
+    case 'partly-cloudy-night':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/partly-cloudy-night.jpg")`
       );
       break;
-    case "rain":
+    case 'rain':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/rain.jpg")`
       );
       break;
-    case "snow":
+    case 'snow':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/snow.jpg")`
       );
       break;
-    case "showers-day":
+    case 'showers-day':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/showers-day.jpg")`
       );
       break;
-    case "showers-nigh":
+    case 'showers-nigh':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/showers-nigh.jpg")`
       );
       break;
-    case "thunder-rain":
+    case 'thunder-rain':
       document.body.style.setProperty(
-        "--background-image",
+        '--background-image',
         `url("/bgImages/thunder-rain.jpg")`
       );
       break;
-  
   }
 }
 
-
-
-
 //execute onclick by pressing enter
-input.addEventListener("keypress", function(event) {
-  
-  if (event.key === "Enter") {
- 
+input.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
     event.preventDefault();
-    
-    document.getElementById("myBtn").click();
+
+    document.getElementById('myBtn').click();
   }
 });
 
 //load london weather when page loads
 
-document.addEventListener("DOMContentLoaded", function() {
-  getData("London");
+document.addEventListener('DOMContentLoaded', function () {
+  getData('London');
 });
